0b708ec968e8daec9eec566f6ed8b8d70ea50031
AndoniSalsidua <157578137+AndoniSalsidua@users.noreply.github.com>
2024-05-07 13:41:29 +0200

    Update Main.java

--- ERRONKA4/src/Main/Main.java
+++ ERRONKA4/src/Main/Main.java
@@ -10,7 +10,10 @@ import Vista.MenuaVi;
 public class Main {
 
     public static void main(String[] args) {
-       
+       int zbk = 0;
+        while(zbk== 0){
+            System.out.println("mentxaaaaaaaaaakaaa")
+        }
         LogInVi frame = new LogInVi();
         frame.setVisible(true);
      
