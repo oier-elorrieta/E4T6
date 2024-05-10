c4752cdb0317d4901e263ca5cd4195713f74a8af
IkerC <iker.corteste@elorrieta-errekamari.com>
2024-05-10 13:14:19 +0200

    erreproduktorea hasi

--- ERRONKA4/src/DB/DBErreproduktorea.java
+++ ERRONKA4/src/DB/DBErreproduktorea.java
@@ -1,20 +1,20 @@
 package DB;
 
+import java.awt.Desktop;
 import java.io.File;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class DBErreproduktorea {
 
     // Audio guztien zerrenda atera
-    public static List<String> IzenaLortu() {
-        List<String> IzenLista = new ArrayList<>();
+    public static List<String> IdLortu() {
+        List<String> IdLista = new ArrayList<>();
         Connection connection = null;
         PreparedStatement statement = null;
         ResultSet resultSet = null;
@@ -23,13 +23,13 @@ public class DBErreproduktorea {
             Connection conn = Konexioa.konektatu();
 
             // SQL-ren audio lista atera gero konparatu ahal izateko
-            String query = "SELECT Izena FROM Audioa";
+            String query = "SELECT IDAudio FROM Audioa";
             statement = connection.prepareStatement(query);
             resultSet = statement.executeQuery();
 
             // Emaitzak rekorritu eta audioekin konparatu
             while (resultSet.next()) {
-            	IzenLista.add(resultSet.getString("Izena"));
+                IdLista.add(resultSet.getString("Izena"));
             }
 
         } catch (SQLException e) {
@@ -44,24 +44,24 @@ public class DBErreproduktorea {
             }
         }
 
-        return IzenLista;
+        return IdLista;
     }
 
     public static File[] AudioMediaAtera() {
         // SQL-ko izenen zerrenda lortu
-        List<String> mediaIzenak = IzenaLortu();
+        List<String> Idak = IdLortu();
 
         // File objetuen array
         List<File> Artxiboak = new ArrayList<>();
 
         // Beste karpeta batean daudenez eta izena ez denez berdina, bidea sortu behar da eskuz
-        for (String mediaIzena : mediaIzenak) {
-            String path = "media/" + mediaIzena + ".wav";
+        for (String Id : Idak) {
+            String path = "media/" + Id + ".wav";
             File file = new File(path);
             if (file.exists()) {
                 Artxiboak.add(file);
             } else {
-                System.err.println("El archivo no existe: " + path);
+                System.err.println(path + "fitxategia ez da agertzen: ");
             }
         }
 
@@ -70,7 +70,17 @@ public class DBErreproduktorea {
 
         return audioFiles;
     }
-    
-  
-    
+
+    public static void playEmon(File archivoAudio) {
+        try {
+            // Verificar que el archivo de audio exista
+            if (archivoAudio.exists()) {
+                Desktop.getDesktop().open(archivoAudio);
+            } else {
+                System.err.println("El archivo de audio no existe.");
+            }
+        } catch (IOException e) {
+            System.err.println("Error al reproducir el audio: " + e.getMessage());
+        }
+    }
 }
--- ERRONKA4/src/Vista/ErreproduktoreaVi.java
+++ ERRONKA4/src/Vista/ErreproduktoreaVi.java
@@ -4,80 +4,76 @@ import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.nio.file.Files;
-import java.util.List;
-
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-
-import DB.DBErreproduktorea;
-import DB.DBLogIn;
-
-import javax.swing.JButton;
 import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+import DB.DBErreproduktorea;
 
 public class ErreproduktoreaVi extends JFrame {
 
-	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
-	private JTextField textField;
-	
+    private static final long serialVersionUID = 1L;
+    private JPanel contentPane;
+    private JTextField textField;
+    private File Aukera; 
+    /**
+     * Create the frame.
+     */
+    public ErreproduktoreaVi(String erabiltzailea, File Aukera) {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 450, 300);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
-	
-	/**
-	 * Create the frame.
-	 */
-	public ErreproduktoreaVi(String erabiltzailea) {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
 
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		JButton menuaBtn = new JButton("Menua");
-		menuaBtn.setBounds(10, 168, 89, 23);
-		contentPane.add(menuaBtn);
-		
-		JButton atzeraBtn = new JButton("<");
-		atzeraBtn.setBounds(109, 168, 41, 23);
-		contentPane.add(atzeraBtn);
-		
-		JButton btnNewButton_2 = new JButton("New button");
-		btnNewButton_2.setBounds(170, 168, 89, 23);
-		contentPane.add(btnNewButton_2);
-		
-		JButton aurreraBtn = new JButton(">");
-		aurreraBtn.setBounds(284, 168, 41, 23);
-		contentPane.add(aurreraBtn);
-		
-		JButton gustokoaBtn = new JButton("Gustokoa");
-		gustokoaBtn.setBounds(335, 168, 89, 23);
-		contentPane.add(gustokoaBtn);
-		
-		textField = new JTextField();
-		textField.setBounds(13, 202, 411, 48);
-		contentPane.add(textField);
-		textField.setColumns(10);
-		
-		JButton profilaBtn = new JButton(erabiltzailea);
-		profilaBtn.setBounds(345, 11, 89, 23);
-		contentPane.add(profilaBtn);
-		profilaBtn.addActionListener(new ActionListener() {
-		    public void actionPerformed(ActionEvent e) {
-		       
-		    	ProfilaVi profilaFrame = new ProfilaVi(erabiltzailea);
+        JButton menuaBtn = new JButton("Menua");
+        menuaBtn.setBounds(10, 168, 89, 23);
+        contentPane.add(menuaBtn);
+
+        JButton atzeraBtn = new JButton("<");
+        atzeraBtn.setBounds(109, 168, 41, 23);
+        contentPane.add(atzeraBtn);
+
+        JButton playBtn = new JButton("Play");
+        playBtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                
+                if (Aukera != null && Aukera.exists()) {
+                    DBErreproduktorea.playEmon(Aukera);
+                } else {
+                    System.err.println("Ez da abestirik aukeratu");
+                }
+            }
+        });
+        playBtn.setBounds(170, 168, 89, 23);
+        contentPane.add(playBtn);
+
+        JButton aurreraBtn = new JButton(">");
+        aurreraBtn.setBounds(284, 168, 41, 23);
+        contentPane.add(aurreraBtn);
+
+        JButton gustokoaBtn = new JButton("Gustokoa");
+        gustokoaBtn.setBounds(335, 168, 89, 23);
+        contentPane.add(gustokoaBtn);
+
+        textField = new JTextField();
+        textField.setBounds(13, 202, 411, 48);
+        contentPane.add(textField);
+        textField.setColumns(10);
+
+        JButton profilaBtn = new JButton(erabiltzailea);
+        profilaBtn.setBounds(345, 11, 89, 23);
+        contentPane.add(profilaBtn);
+        profilaBtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                ProfilaVi profilaFrame = new ProfilaVi(erabiltzailea);
                 profilaFrame.setVisible(true);
-		    	
-		    }
-		    
-		    
-		    
-		});
-		List<String> izenburuak = DBErreproduktorea.IzenaLortu();
-	    File[] abestiak =DBErreproduktorea.AudioMediaAtera();
-	}
-	
+            }
+        });
+
+        this.Aukera = Aukera;
+    }
 }
