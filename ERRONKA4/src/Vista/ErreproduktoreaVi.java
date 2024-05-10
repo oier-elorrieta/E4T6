d0f9f4d2944f6dd1666818547d4bd1660f367374
IkerC <iker.corteste@elorrieta-errekamari.com>
2024-05-10 09:39:31 +0200

    erreproduktorean aldaketak

--- ERRONKA4/src/Vista/ErreproduktoreaVi.java
+++ ERRONKA4/src/Vista/ErreproduktoreaVi.java
@@ -16,22 +16,7 @@ public class ErreproduktoreaVi extends JFrame {
 	private JPanel contentPane;
 	private JTextField textField;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					ErreproduktoreaVi frame = new ErreproduktoreaVi("NombreUsuario");
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
+	
 	/**
 	 * Create the frame.
 	 */
