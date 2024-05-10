a6ad962f60fffeda9a35e75f1a47924015272d29
IkerC <iker.corteste@elorrieta-errekamari.com>
2024-05-10 09:27:44 +0200

    erreproduktorean aldaketak

--- ERRONKA4/src/DB/DBErreproduktorea.java
+++ ERRONKA4/src/DB/DBErreproduktorea.java
@@ -6,7 +6,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class DBErreproduktorea {
 
@@ -68,4 +70,38 @@ public class DBErreproduktorea {
 
         return audioFiles;
     }
+    
+    public static Map<String, File> obtenerCancionesConArchivos() {
+        // Abesti lista lortu datubasetik
+        List<String> AbestiIzenak = MediaLortu();
+
+       // Audioen fitxategiak lortu
+        File[] AudioFitxategiak = AudioMediaAtera();
+
+        // Crear un HashMap para mapear las canciones con sus archivos de audio
+        Map<String, File> IzenEtaFitxategi = new HashMap<>();
+
+        // Verificar que el tamaño de ambas listas sea el mismo
+        if (AbestiIzenak.size() == AudioFitxategiak.length) {
+            // Asociar cada canción con su respectivo archivo de audio
+            for (int i = 0; i < AbestiIzenak.size(); i++) {
+                String AbestiIzena = AbestiIzenak.get(i);
+                File AudioFitxategi = AudioFitxategiak[i];
+
+                // Verificar si la canción ya está enlazada con otro archivo de audio
+                if (IzenEtaFitxategi.containsKey(AbestiIzena)) {
+                    System.out.println(AbestiIzena + " audio fitxategiarekin bat dago");
+                } else {
+                    // Asociar la canción con su archivo de audio
+                	IzenEtaFitxategi.put(AbestiIzena, AudioFitxategi);
+                }
+            }
+        } else {
+            System.out.println("Abesti kantitatea eta audioen fitxategia EZ da berdina");
+        }
+
+        return IzenEtaFitxategi;
+    }
+    
+    
 }
